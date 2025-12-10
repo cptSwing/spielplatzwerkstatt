d0a853a19ca033dbf3ce23406996bc0ac336dce6
cptSwing <jens@jbrandenburg.de>
2025-12-10 15:11:33 +0100

    style name changes

--- partials/footer.php
+++ partials/footer.php
@@ -1,4 +1,4 @@
-<footer class="w-full h-(--header-footer-height)  mt-(--header-footer-margin) px-(--container-horizontal-margin)  [clip-path:polygon(0%_var(--header-footer-offset),100%_0%,100%_100%,0%_100%)] bg-theme-logo-4 text-theme-logo-1 flex flex-row justify-between items-center">
+<footer class="w-full h-(--header-footer-height)  mt-(--header-footer-margin) px-(--container-horizontal-margin)  [clip-path:polygon(0%_var(--header-footer-offset),100%_0%,100%_100%,0%_100%)] bg-theme-hellgruen text-theme-dunkelgruen flex flex-row justify-between items-center">
     <p class="text-sm whitespace-pre-line">
         Impressum
         lalala ldfsdfds
