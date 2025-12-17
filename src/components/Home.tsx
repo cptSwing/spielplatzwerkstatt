e50bcc5e1fd149658f781659594669d0892f2f39
cptSwing <jens@jbrandenburg.de>
2025-12-17 18:24:30 +0100

    header size reduction on mobile

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -92,7 +92,7 @@ const Home = () => {
                             <h5 className="relative z-0 w-fit pr-10 pl-(--content-card-padding) text-theme-background before:absolute before:top-0 before:left-0 before:-z-10 before:size-full before:bg-theme-secondary">
                                 Garten- & Landschaftsbau
                             </h5>
-                            <hr className="-mt-(--content-card-padding-half) w-[calc(100%-var(--anchored-content-hr-padding)+var(--content-card-padding-double))] text-theme-secondary" />
+                            <hr className="-mt-(--content-card-padding-half) w-[calc(100%-var(--anchored-content-hr-padding)+var(--content-card-padding-half))] text-theme-secondary" />
                         </div>
 
                         <div className="text-justify text-pretty text-neutral-600">
@@ -268,40 +268,56 @@ const AnchorLinks = () => {
         <div className="-mt-(--content-card-padding-double) flex min-h-(--page-height-no-header-no-footer) w-full flex-col items-stretch justify-center gap-(--content-card-padding-double) sm:w-4/5 md:w-3/5 lg:w-1/2 2xl:w-1/3">
             <a
                 href="#home-anchor-spielplatzbau"
-                className="element-level-1 group relative flex h-16 flex-row items-center justify-start gap-(--content-card-padding) border-theme-primary-variation bg-theme-primary p-(--content-card-padding)"
+                className="element-level-1 group relative flex h-16 flex-row items-center justify-start gap-(--content-card-padding) border-theme-primary-variation bg-theme-primary p-(--content-card-padding) no-underline hover:bg-theme-primary-variation active:bg-theme-primary-variation"
             >
                 <div className="aspect-square h-[150%]">
-                    <img className="image-hover object-cover drop-shadow-md" alt="spielplatzbau" src="../images/symbol_geraet_238x240.png" />
+                    <img
+                        className="image-hover object-cover drop-shadow-centered-sm drop-shadow-theme-primary-variation/75"
+                        alt="spielplatzbau"
+                        src="../images/symbol_geraet_238x240.png"
+                    />
                 </div>
                 <h4 className="text-center text-theme-background">Spielplatzbau</h4>
             </a>
 
             <a
                 href="#home-anchor-galabau"
-                className="element-level-1 group relative flex h-16 flex-row items-center justify-start gap-(--content-card-padding) border-theme-primary-variation bg-theme-primary p-(--content-card-padding)"
+                className="element-level-1 group relative flex h-16 flex-row items-center justify-start gap-(--content-card-padding) border-theme-primary-variation bg-theme-primary p-(--content-card-padding) no-underline hover:bg-theme-primary-variation active:bg-theme-primary-variation"
             >
                 <div className="aspect-square h-[150%]">
-                    <img className="image-hover object-cover drop-shadow-md" alt="galabau" src="../images/symbol_spaten_203x205.png" />
+                    <img
+                        className="image-hover object-cover drop-shadow-centered-sm drop-shadow-theme-primary-variation/75"
+                        alt="galabau"
+                        src="../images/symbol_spaten_203x205.png"
+                    />
                 </div>
                 <h4 className="text-center text-theme-background">Garten- & Landschaftsbau</h4>
             </a>
 
             <a
                 href="#home-anchor-naschgarten"
-                className="element-level-1 group relative flex h-16 flex-row items-center justify-start gap-(--content-card-padding) border-theme-primary-variation bg-theme-primary p-(--content-card-padding)"
+                className="element-level-1 group relative flex h-16 flex-row items-center justify-start gap-(--content-card-padding) border-theme-primary-variation bg-theme-primary p-(--content-card-padding) no-underline hover:bg-theme-primary-variation active:bg-theme-primary-variation"
             >
                 <div className="aspect-square h-[150%]">
-                    <img className="image-hover object-cover drop-shadow-md" alt="naschgarten" src="../images/symbol_baum_211x211.png" />
+                    <img
+                        className="image-hover object-cover drop-shadow-centered-sm drop-shadow-theme-primary-variation/75"
+                        alt="naschgarten"
+                        src="../images/symbol_baum_211x211.png"
+                    />
                 </div>
                 <h4 className="text-center text-theme-background">Naschgarten</h4>
             </a>
 
             <a
                 href="#home-anchor-workshops"
-                className="element-level-1 group relative flex h-16 flex-row items-center justify-start gap-(--content-card-padding) border-theme-primary-variation bg-theme-primary p-(--content-card-padding)"
+                className="element-level-1 group relative flex h-16 flex-row items-center justify-start gap-(--content-card-padding) border-theme-primary-variation bg-theme-primary p-(--content-card-padding) no-underline hover:bg-theme-primary-variation active:bg-theme-primary-variation"
             >
                 <div className="aspect-square h-[150%]">
-                    <img className="image-hover object-cover drop-shadow-md" alt="workshops" src="../images/symbol_buch_210x213.png" />
+                    <img
+                        className="image-hover object-cover drop-shadow-centered-sm drop-shadow-theme-primary-variation/75"
+                        alt="workshops"
+                        src="../images/symbol_buch_210x213.png"
+                    />
                 </div>
                 <h4 className="text-center text-theme-background">Workshops</h4>
             </a>
@@ -311,29 +327,32 @@ const AnchorLinks = () => {
 
 const NewsItems = () => {
     return (
-        <div className="element-level-1 p-(--content-card-padding)">
-            <h2 className="text-center">Neuigkeiten:</h2>
+        <div className="element-level-1 w-(--inner-container-width) p-(--content-card-padding-double)">
+            <h2 className="mb-(--content-card-padding) text-center">Neuigkeiten:</h2>
 
-            <div className="mx-auto grid w-full grid-cols-1 gap-(--content-card-padding-double) [--news-card-padding:calc(var(--content-card-padding)/2)] md:grid-cols-3">
-                <div className="element-level-2 h-auto overflow-hidden p-(--news-card-padding)">
-                    <h3>Newsitem 1</h3>
-                    <p className="text-sm">
+            <div className="mx-auto grid w-full grid-cols-1 gap-(--content-card-padding-double) md:grid-cols-3">
+                <div className="element-level-2 h-auto cursor-pointer overflow-hidden p-(--content-card-padding) select-none hover:bg-theme-primary/90 active:bg-theme-primary/90">
+                    <h4 className="mb-(--content-card-padding-half) text-white">Newsitem 1</h4>
+                    <p className="text-xs text-theme-background">
+                        Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt..
+                        Lorem Ipsum Excerpt.. Lorem Ipsum Excerpt.. Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt...
+                        Lorem Ipsum Excerpt... Lorem Ipsum Excerpt.. Lorem Ipsum Excerpt.. Lorem Ipsum Excerpt.. Lorem Ipsum Excerpt... Lorem Ipsum Excerpt...
+                        Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt.. Lorem Ipsum Excerpt.. Lorem Ipsum Excerpt..
+                    </p>
+                </div>
+                <div className="element-level-2 h-auto cursor-pointer overflow-hidden p-(--content-card-padding) select-none hover:bg-theme-primary/90 active:bg-theme-primary/90">
+                    <h4 className="mb-(--content-card-padding-half) text-white">Newsitem 2</h4>
+                    <p className="text-xs text-theme-background">
                         Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt..
                         Lorem Ipsum Excerpt.. Lorem Ipsum Excerpt..
                     </p>
                 </div>
-                <div className="element-level-2 h-auto overflow-hidden p-(--news-card-padding)">
-                    <h3>Newsitem 2</h3>
-                    <p className="text-sm">
+                <div className="element-level-2 h-auto cursor-pointer overflow-hidden p-(--content-card-padding) select-none hover:bg-theme-primary/90 active:bg-theme-primary/90">
+                    <h4 className="mb-(--content-card-padding-half) text-white">Newsitem 3</h4>
+                    <p className="text-xs text-theme-background">
                         Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt..
-                        Lorem Ipsum Excerpt.. Lorem Ipsum Excerpt..
-                    </p>
-                </div>
-                <div className="element-level-2 h-auto overflow-hidden p-(--news-card-padding)">
-                    <h3>Newsitem 3</h3>
-                    <p className="text-sm">
-                        Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt..
-                        Lorem Ipsum Excerpt.. Lorem Ipsum Excerpt..
+                        Lorem Ipsum Excerpt.. Lorem Ipsum Excerpt.. Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt... Lorem Ipsum Excerpt...
+                        Lorem Ipsum Excerpt... Lorem Ipsum Excerpt.. Lorem Ipsum Excerpt.. Lorem Ipsum Excerpt..
                     </p>
                 </div>
             </div>
