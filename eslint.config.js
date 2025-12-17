a2c3aba4467b277a0070a0790437915b4ef40b39
cptSwing <jens@jbrandenburg.de>
2025-12-17 17:42:50 +0100

    linting css files, and more precise eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -3,28 +3,68 @@ import globals from 'globals';
 import tsEslint from 'typescript-eslint';
 import prettierEslintRecommended from 'eslint-plugin-prettier/recommended';
 import jsxA11yEslint from 'eslint-plugin-jsx-a11y';
-import preactEsLint from 'eslint-config-preact';
-import htmlEsLint from '@html-eslint/eslint-plugin';
+import preactEslint from 'eslint-config-preact';
+import htmlEslint from '@html-eslint/eslint-plugin';
+import cssEslint from '@eslint/css';
+import { tailwind4 } from 'tailwind-csstree';
 
 export default [
     {
-        ignores: ['dist/**/*', 'node_modules'],
-    },
-
-    htmlEsLint.configs?.recommended,
-    jsEslint.configs.recommended,
-    ...preactEsLint,
-    ...tsEslint.configs.recommended,
-    jsxA11yEslint.flatConfigs.recommended,
-    prettierEslintRecommended,
-
-    {
-        files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+        name: 'languageOptions',
         languageOptions: {
+            parserOptions: {
+                ecmaVersion: 2020,
+                sourceType: 'module',
+                ecmaFeatures: {
+                    modules: true,
+                    jsx: true,
+                },
+            },
             globals: {
                 ...globals.node,
             },
         },
+    },
+
+    {
+        name: 'ignores',
+        ignores: ['dist/**/*', 'node_modules'],
+    },
+
+    {
+        name: '@eslint/css/recommended',
+        ...cssEslint.configs.recommended,
+        files: ['**/*.css'],
+        language: 'css/css',
+        languageOptions: {
+            customSyntax: tailwind4,
+            tolerant: true,
+        },
+        rules: {
+            'css/no-empty-blocks': 'warn',
+        },
+    },
+
+    {
+        name: '@html-eslint/eslint-plugin/flat/recommended',
+        ...htmlEslint.configs?.['flat/recommended'],
+        files: ['**/*.html'],
+        rules: {
+            'html/no-duplicate-class': 'error',
+        },
+    },
+
+    // Before jsEslint since it loads default jsElint rules, sigh
+    {
+        name: 'eslint-config-preact',
+        ...preactEslint[1],
+        files: ['**/*.{ts,tsx}'],
+    },
+
+    {
+        name: '@eslint/js/recommended',
+        ...jsEslint.configs.recommended,
+        files: ['**/*.{ts,tsx}'],
         rules: {
             'no-empty': 'off',
             'object-shorthand': 'warn',
@@ -32,7 +72,14 @@ export default [
             'no-unused-vars': 'off',
             'no-unused-expressions': 'off',
             'no-unreachable': 'warn',
+        },
+    },
 
+    {
+        name: 'typescript-eslint/recommended',
+        ...tsEslint.configs.recommended[0],
+        files: ['**/*.{ts,tsx}'],
+        rules: {
             '@typescript-eslint/no-unused-vars': [
                 'warn',
                 {
@@ -50,10 +97,10 @@ export default [
     },
 
     {
-        files: ['**/*.{html}'],
-        plugins: { htmlEsLint },
-        rules: {
-            'html/no-duplicate-class': 'error',
-        },
+        name: 'eslint-plugin-jsx-a11y/recommended',
+        ...jsxA11yEslint.flatConfigs.recommended,
+        files: ['**/*.{ts,tsx}'],
     },
+
+    prettierEslintRecommended,
 ];
