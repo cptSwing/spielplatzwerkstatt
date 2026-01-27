279a6f9f56fae2d403a7c5e20c70b91e51746d71
cptSwing <jens@jbrandenburg.de>
2026-01-27 12:25:09 +0100

    added spinner for loading times

--- src/components/LoadingMessage.tsx
+++ src/components/LoadingMessage.tsx
@@ -1,9 +1,15 @@
+import { classNames } from 'cpts-javascript-utilities';
+
 const LoadingMessage = () => {
     return (
         <main className="flex items-center justify-center">
-            <div className="animate-bounce rounded-lg bg-white p-2 text-neutral-400">Lade Inhalte...</div>
+            <LoadingSpinner classes="text-theme-primary" />
         </main>
     );
 };
 
 export default LoadingMessage;
+
+export const LoadingSpinner = ({ classes }: { classes?: string }) => {
+    return <div className={classNames('size-10 animate-spin bg-[currentColor] [mask:url("/svg/ArrowPathRoundedOutline.svg")]', classes && classes)} />;
+};
--- src/components/NewsItems.tsx
+++ src/components/NewsItems.tsx
@@ -5,6 +5,7 @@ import { classNames, keyDownA11y } from 'cpts-javascript-utilities';
 import type { TargetedKeyboardEvent } from 'preact';
 import type { ACF_Nachricht_Type } from '../types/types';
 import { useBreakpoint } from '../hooks/useBreakpoint';
+import { LoadingSpinner } from './LoadingMessage';
 
 const newsItemsPerBreakpoint = new Map([
     [null, 1],
@@ -17,7 +18,12 @@ const newsItemsPerBreakpoint = new Map([
 
 const NewsItems = () => {
     const breakpoint = useBreakpoint();
-    const newsItemsPerPage = newsItemsPerBreakpoint.get(breakpoint)!;
+    const [newsItemsPerPage, setnewsItemsPerPage] = useState(1);
+    useEffect(() => {
+        if (breakpoint) {
+            setnewsItemsPerPage(newsItemsPerBreakpoint.get(breakpoint)!);
+        }
+    }, [breakpoint]);
 
     const [newsPage, setNewsPage] = useState(1);
     const { newsHaveLoaded, news, totalPages, totalPosts: _totalPosts } = useFetchNews(newsPage, newsItemsPerPage);
@@ -137,23 +143,9 @@ const NewsItems = () => {
                         : Array.from({ length: newsItemsPerPage }, (_, idx) => (
                               <div
                                   key={idx}
-                                  className="flex items-center justify-center element-level-2 p-(--content-card-padding) text-left text-theme-primary opacity-60 select-none"
+                                  className="flex items-center justify-center element-level-2 p-(--content-card-padding) text-theme-primary opacity-80 select-none"
                               >
-                                  <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      fill="none"
-                                      viewBox="0 0 24 24"
-                                      strokeWidth={1.5}
-                                      stroke="currentColor"
-                                      className="size-12 animate-spin"
-                                  >
-                                      <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                      <path
-                                          strokeLinecap="round"
-                                          strokeLinejoin="round"
-                                          d="M9 9.563C9 9.252 9.252 9 9.563 9h4.874c.311 0 .563.252.563.563v4.874c0 .311-.252.563-.563.563H9.564A.562.562 0 0 1 9 14.437V9.564Z"
-                                      />
-                                  </svg>
+                                  <LoadingSpinner />
                               </div>
                           )).map((elem) => elem)}
                 </div>
