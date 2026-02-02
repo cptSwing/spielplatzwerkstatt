85cbd08b677392a4abd40658ff9c55239bcd57e0
Jens <jens@vragments.com>
2026-02-02 17:53:19 +0100

    style change footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,9 +1,9 @@
 
 <footer class="mt-(--header-footer-margin) flex min-h-(--header-footer-height) w-full flex-row items-center justify-between bg-theme-primary-variation px-(--container-horizontal-margin) text-theme-text [clip-path:var(--clip-path-angled-top)]">
-    <div class=" ">
-        <ul>
-            <li class="my-1"><a href="/pages/impressum.php" class="text-xs italic w-full block text-left">Impressum</a></li>
-            <li class="my-1"><a href="/pages/kontakt.php" class="text-xs italic w-full block text-left">Kontakt</a></li>
+    <div>
+        <ul class="ml-0 list-inside">
+            <li class="my-1"><a href="/pages/impressum.php" class="text-xs italic">Impressum</a></li>
+            <li class="my-1"><a href="/pages/kontakt.php" class="text-xs italic">Kontakt</a></li>
         </ul>
     </div>
 
@@ -11,10 +11,10 @@
         <div class="font-gabarito italic text-theme-text/50 text-right text-xs w-full my-1">Unsere Partner:</div>
         <div class="flex items-end justify-start flex-col gap-0.5 mb-2">
             <div class="xl:w-3/4 ml-auto mr-0 w-1/3 md:w-1/2 object-contain">
-                <img src="/images/spielplatzwerkstatt_grueber_logo_klein.png" />
+                <img src="/images/spielplatzwerkstatt_grueber_logo_klein.png" class="w-fit max-h-8" />
             </div>
             <div class="xl:w-3/4 ml-auto mr-0 w-1/3 md:w-1/2 object-contain">
-                <img src="/images/wamiki_logo_klein.png" />
+                <img src="/images/wamiki_logo_klein.png" class="w-fit max-h-8" />
             </div>
         </div>
     </div>
