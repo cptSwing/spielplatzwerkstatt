0e1ca327992a0df09ccbdec3e3ebeeb233eb5706
Jens <jens@vragments.com>
2026-02-03 18:06:10 +0100

    prettier-php lint

--- partials/footer.php
+++ partials/footer.php
@@ -2,12 +2,16 @@
 <footer class="mt-(--header-footer-margin) flex min-h-(--header-footer-height) w-full flex-row items-center justify-between bg-theme-primary-variation px-(--container-horizontal-margin) text-theme-text/50 [clip-path:var(--clip-path-angled-top)] text-xs">
     <div>
         <ul class="ml-0 list-inside">
-            <li class="my-1.5"><a href="/pages/impressum.php" class="ml-px inline-block no-underline px-1 hover-active:text-theme-background hover-active:outline-theme-accent outline-1 outline-transparent outline-offset-1 duration-100  transition-[color,outline-color] <?php if ($route == 'impressum') {
-                        echo 'outline-theme-accent! text-theme-background!';
-                    } ?>" >Impressum</a></li>
-            <li class="my-1.5"><a href="/pages/kontakt.php" class="ml-px inline-block no-underline px-1 hover-active:text-theme-background hover-active:outline-theme-accent outline-1 outline-transparent outline-offset-1 duration-100  transition-[color,outline-color] <?php if ($route == 'kontakt') {
-                        echo 'outline-theme-accent! text-theme-background!';
-                    } ?>">Kontakt</a></li>
+            <li class="my-1.5"><a href="/pages/impressum.php" class="ml-px inline-block no-underline px-1 hover-active:text-theme-background hover-active:outline-theme-accent outline-1 outline-transparent outline-offset-1 duration-100  transition-[color,outline-color] <?php if (
+                $route == 'impressum'
+            ) {
+                echo 'outline-theme-accent! text-theme-background!';
+            } ?>" >Impressum</a></li>
+            <li class="my-1.5"><a href="/pages/kontakt.php" class="ml-px inline-block no-underline px-1 hover-active:text-theme-background hover-active:outline-theme-accent outline-1 outline-transparent outline-offset-1 duration-100  transition-[color,outline-color] <?php if (
+                $route == 'kontakt'
+            ) {
+                echo 'outline-theme-accent! text-theme-background!';
+            } ?>">Kontakt</a></li>
         </ul>
     </div>
 
