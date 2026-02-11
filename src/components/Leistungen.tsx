03a995a10938fd3aa853b04791db191e26179be4
Jens <jens@vragments.com>
2026-02-11 16:44:20 +0100

    better visibilty verification

--- src/components/Leistungen.tsx
+++ src/components/Leistungen.tsx
@@ -16,7 +16,9 @@ const Leistungen = ({ leistungsData, route }: { leistungsData: ACF_Leistung_Type
         infokarte_4,
         infokarte_5,
         produkt_story_a,
+        zweite_produkt_story,
         produkt_story_b,
+        dritte_produkt_story,
         produkt_story_c,
     } = leistungsData;
 
@@ -48,8 +50,8 @@ const Leistungen = ({ leistungsData, route }: { leistungsData: ACF_Leistung_Type
             <TextBlock textBlockData={banderole} />
             <InfoKarten kartenData={{ infokarte_1, infokarte_2, infokarte_3, infokarte_4, infokarte_5 }} />
             {produkt_story_a && <ProduktStory storyData={produkt_story_a} />}
-            {produkt_story_b && <ProduktStory storyData={produkt_story_b} />}
-            {produkt_story_c && <ProduktStory storyData={produkt_story_c} />}
+            {zweite_produkt_story && produkt_story_b && <ProduktStory storyData={produkt_story_b} />}
+            {dritte_produkt_story && produkt_story_c && <ProduktStory storyData={produkt_story_c} />}
         </main>
     );
 };
